07abb20e3ed3439df549819ad179f030d66e557a
Ana Sofia <anasofiaricardoaguilar@gmail.com>
2022-11-04 13:53:18 -0500

    instrumento evaluativo nota final

---
+++ script.js
@@ -0,0 +1,18 @@
+const name = document.getElementById(Name);
+const pname = document.getElementById(Pname)
+const numberone = toString(document.getElementById(Numberone));
+const numbertwo = toString(document.getElementById(Numbertwo));
+const numbertree = toString(document.getElementById(Numbertree));
+const final = toString(document.getElementById(NotaFinal));
+const btn = document.getElementById(btncalculator);
+
+  // btn.addEventListener("click",()=>{
+ 
+  // });
+
+  function calcula(){
+   
+  }
+  
+
+
